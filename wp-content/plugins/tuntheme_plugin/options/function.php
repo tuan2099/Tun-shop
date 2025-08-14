46e0e4997a16a55794d280ec0fc460d28c22fad3
tuan2099 <gwoqkepspsld@gmail.com>
2025-08-15 00:07:33 +0700

    update

---
+++ wp-content/plugins/tuntheme_plugin/options/function.php
@@ -0,0 +1,3 @@
+<?php 
+
+$theme = wp_get_theme(); // Lấy theme hiện tại
---
+++ wp-content/plugins/tuntheme_plugin/plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Tun Plugin
+Plugin URI:
+Description: This plugin Only competible With Tun Theme By TunTheme.
+Version: 1.1.3
+Author: TunTheme (Rabie Elkheir)
+Author Email: support@Tunthemes.com
+
+  Copyright 2018 TunTheme
+*/
